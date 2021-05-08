613aa55fee3477141756166c28fb09bdc0977532
Alexjandrohum <alexjandrohum@MiMac.local>
2021-05-08 11:40:49 -0500

    Init proyect

---
+++ src/main/java/com/clients/admin/AdminClientsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.clients.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClientsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminClientsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clients/admin/AdminClientsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clients.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminClientsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
